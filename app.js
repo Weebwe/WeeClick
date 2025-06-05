13f7a746f3bb7064bfc631d34511c47259a901a7
Weebwe LLC <weeb.llc@gmail.com>
2025-06-05 13:01:57 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+import { TonConnectUI } from "https://unpkg.com/@tonconnect/ui@latest/dist/tonconnect-ui.min.js";
+
+const tonConnectUI = new TonConnectUI({
+  manifestUrl: "https://weebwe.github.io/WeeClick/tonconnect-manifest.json",
+  buttonRootId: "ton-connect-button"
+});
