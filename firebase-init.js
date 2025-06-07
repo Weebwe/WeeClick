25542d9ce59e604b2b8c8c5c18399011e5bcd251
Weebwe LLC <weeb.llc@gmail.com>
2025-06-07 23:37:51 +0300

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCS4Kae1c1hDf7KVfKAQp3UwKDeRCq3HRA",
+  authDomain: "weeclick-85bd9.firebaseapp.com",
+  projectId: "weeclick-85bd9",
+  storageBucket: "weeclick-85bd9.firebasestorage.app",
+  messagingSenderId: "793288874462",
+  appId: "1:793288874462:web:bb5db998662295df3c654f"
+};
+
+const app = initializeApp(firebaseConfig);
